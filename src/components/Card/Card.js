ccfe4f161a65189d6da909223134249fc1e2acbd
Hugo Cullinan <hugo.cullinan@gmail.com>
2024-12-19 12:44:18 +1100

    change card s and add all

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,13 +4,8 @@ const Card = ({ name, img, role, region, power, health }) => {
   const roleClass = `card-${role.toLowerCase()}`;
 
   return (
-    <div className={`card ${roleClass}`}>
-      <h2>{name}</h2>
+    <div className={"card"}>
       <img className="card-img" src={img} />
-      <h3 className="card-power">Power: {power}</h3>
-      <h3 className="card-health">Health: {health}</h3>
-
-      <h3>Region: {region}</h3>
     </div>
   );
 };
